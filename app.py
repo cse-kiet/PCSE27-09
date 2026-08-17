d84e7acc5619ca8c420bc62e92201a9bcbb8f959
Pearl Kayastha <pearl.gkp@gmail.com>
2026-08-17 17:13:07 +0530

    Initial Major Project Commit - Crime Analysis AI

---
+++ app.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask, render_template, request, url_for, jsonify
+from markupsafe import Markup
+
+# Initialize the Flask App
+app = Flask(__name__)
+
+from routes import *
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
